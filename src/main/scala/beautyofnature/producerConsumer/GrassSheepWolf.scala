d0eb0058ea45f90e0b02cecbf61a03a8c9687445
anastasia <alazareva@luciddg.com>
2020-09-21 18:28:35 -0700

    gsw

--- src/main/scala/beautyofnature/producerConsumer/GrassSheepWolf.scala
+++ src/main/scala/beautyofnature/producerConsumer/GrassSheepWolf.scala
@@ -1,6 +1,6 @@
 package beautyofnature.producerConsumer
 
-import processing.core.PApplet
+import processing.core.{PApplet, PConstants}
 
 import scala.util.Random
 
@@ -16,6 +16,7 @@ trait GridCell {
   def isSheep: Boolean
   def isWolf: Boolean
   def isEmpty: Boolean
+  def visited: Boolean
 }
 case class FullGridCell(species: Species, visited: Boolean) extends GridCell{
   def isGrass: Boolean = species match {
@@ -41,6 +42,8 @@ case class EmptyGridCell(energy: Float) extends GridCell{
   override def isSheep: Boolean = false
 
   override def isWolf: Boolean = false
+
+  override def visited: Boolean = false
 }
 
 case class GameParameters(row: Int = 100,
@@ -52,30 +55,25 @@ case class GameParameters(row: Int = 100,
                           shepStepCost: Float = 5,
                           wolfStepConst: Float = 5,
                           initialGrass: Int = 5000,
-                          initialSheep: Int = 500,
-                          initialWolf: Int = 50,
-                          minPlants: Int = 3,
+                          initialSheep: Int = 250,
+                          initialWolf: Int = 10,
+                          minPlants: Int = 2,
                          )
 
-case class Game(board: Array[GridCell], parameters: GameParameters) {
+class Game(val board: Array[Array[GridCell]], val parameters: GameParameters) {
 
-  def makeNewGrass(i: Int, j: Int): Boolean = ???
+  def makeNewGrass(i: Int, j: Int): Boolean = indexOfNeighbors(i, j)(c => c.isGrass && !c.visited) match {
+    case Nil => false
+    case list => list.length >= parameters.minPlants
+  }
 
   def updateGrass(): Unit = {
     for {
       i <- 0 until parameters.row
       j <- 0 until parameters.cols
     } board(i)(j) match {
-      case EmptyGridCell(e) if e >= parameters.grassEnergy  => {
-        if (makeNewGrass(i, j)) board(i)(j) = FullGridCell(Grass(parameters.grassEnergy), visited = true)
-      }
-      case _ => ()
-    }
-    for {
-      i <- 0 until parameters.row
-      j <- 0 until parameters.cols
-    } board(i)(j) match {
-      case FullGridCell(g: Grass, true) =>  board(i)(j) = FullGridCell(g, visited = false)
+      case EmptyGridCell(e) if e >= parameters.grassEnergy && makeNewGrass(i, j) =>
+        board(i)(j) = FullGridCell(Grass(parameters.grassEnergy), visited = true)
       case EmptyGridCell(e) => board(i)(j) = EmptyGridCell(e + 1)
       case _ => ()
     }
@@ -85,7 +83,9 @@ case class Game(board: Array[GridCell], parameters: GameParameters) {
     val matches = for {
       ni <- i - 1 to i + 1
       nj <- j - 1 to j + 1
-      if ni != nj && ni >= 0 && ni < parameters.row && nj >= 0 && nj < parameters.cols && predicate(board(ni)(nj))
+      if (ni, nj) != (i, j) && ni >= 0 &&
+        ni < parameters.row && nj >= 0 &&
+        nj < parameters.cols && predicate(board(ni)(nj))
     } yield (ni, nj)
     matches.toList
   }
@@ -135,7 +135,7 @@ case class Game(board: Array[GridCell], parameters: GameParameters) {
       case FullGridCell(Wolf(e), false) if e - parameters.wolfStepConst <= 0 => board(i)(j) = EmptyGridCell(0)
       case FullGridCell(Wolf(e), false) =>
         val newWolf = Wolf(e - parameters.wolfStepConst)
-        randomNeighborIndex(i, j)(_.isSheep) match {
+        randomNeighborIndex(i, j)(c => c.isSheep || c.isGrass) match {
           case Some((ni, nj)) =>
             if (newWolf.energy > parameters.wolfEnergy) {
               board(ni)(nj) = FullGridCell(Wolf(newWolf.energy / 2), visited = true)
@@ -150,11 +150,168 @@ case class Game(board: Array[GridCell], parameters: GameParameters) {
     }
   }
 
+  def updateVisitedFlag(): Unit = for {
+    i <- 0 until parameters.row
+    j <- 0 until parameters.cols
+  } board(i)(j) match {
+    case FullGridCell(a, true) => board(i)(j) = FullGridCell(a, visited = false)
+    case _ => ()
+  }
+
+  def update(): (Int, Int, Int) = {
+    updateVisitedFlag()
+    updateGrass()
+    updateSheep()
+    updateWolves()
+    board.flatten.foldLeft((0, 0, 0)) { case ((g, s, w), cell) => cell match {
+      case _: EmptyGridCell => (g, s, w)
+      case FullGridCell(_: Grass, _) => (g + 1, s, w)
+      case FullGridCell(_: Sheep, _) => (g, s + 1, w)
+      case FullGridCell(_: Wolf, _) => (g, s, w + 1)
+    }
+    }
+  }
+}
+
+object Game {
+
+  def apply(parameters: GameParameters): Game = {
+    val board = Array.ofDim[GridCell](parameters.row, parameters.cols)
+    for {
+      i <- 0 until parameters.row
+      j <- 0 until parameters.cols
+    } board(i)(j) = EmptyGridCell(0)
+
+
+    def emptyCells(board: Array[Array[GridCell]]): List[(Int, Int)] = {
+      val matches = for {
+        i <- 0 until parameters.row
+        j <- 0 until parameters.cols
+        if board(i)(j).isEmpty
+      } yield (i, j)
+      matches.toList
+    }
+
+    def randomEmptyCell(board: Array[Array[GridCell]]): Option[(Int, Int)] = {
+      emptyCells(board) match {
+        case Nil => None
+        case list => Some(list(Random.nextInt(list.size)))
+      }
+    }
+
+    for (_ <-  0 until parameters.initialGrass) {
+      randomEmptyCell(board) match {
+        case None => ()
+        case Some((i, j)) => board(i)(j) = FullGridCell(Grass(parameters.grassEnergy), visited = false)
+      }
+    }
+
+    for (_ <-  0 until parameters.initialSheep) {
+      randomEmptyCell(board) match {
+        case None => ()
+        case Some((i, j)) => board(i)(j) = FullGridCell(Sheep(parameters.sheepEnergy), visited = false)
+      }
+    }
+
+    for (_ <-  0 until parameters.initialWolf) {
+      randomEmptyCell(board) match {
+        case None => ()
+        case Some((i, j)) => board(i)(j) = FullGridCell(Wolf(parameters.wolfEnergy), visited = false)
+      }
+    }
+
+    new Game(board, parameters)
+  }
+}
+
+object GrassSheepWolfParameters {
+  val default = GameParameters()
 }
 
 class GrassSheepWolf extends PApplet {
 
+  val game = Game(GrassSheepWolfParameters.default)
 
+  var counts: List[(Int, Int, Int)] = List.empty[(Int, Int, Int)]
+
+  override def settings(): Unit = {
+    size(800, 900, PConstants.P2D)
+  }
+
+  override def setup(): Unit = {
+    background(255)
+    colorMode(PConstants.RGB, 255, 255, 255, 100)
+    frameRate(3)
+    strokeWeight(0.5f)
+    stroke(200)
+    counts = (game.parameters.initialGrass, game.parameters.initialSheep, game.parameters.initialWolf) :: counts
+  }
+
+  def drawGrid(): Unit = {
+    val diameter = width / game.parameters.row
+    for {
+      i <- 0 until game.parameters.row
+      j <- 0 until game.parameters.cols
+    } {
+      val c = game.board(i)(j) match {
+        case EmptyGridCell(e) => color(255, 255, 255, PApplet.map(e, 0, game.parameters.grassEnergy, 0, 50))
+        case FullGridCell(Grass(e), _) => color(0, 255, 0, PApplet.map(e, 0, game.parameters.grassEnergy, 90, 100))
+        case FullGridCell(Sheep(e), _) => color(0, 0, 255, PApplet.map(e, 0, game.parameters.sheepEnergy, 0, 100))
+        case FullGridCell(Wolf(e), _) => color(255, 0, 0, PApplet.map(e, 0, game.parameters.wolfEnergy, 0, 100))
+      }
+      val x = PApplet.map(j, 0, game.parameters.cols, 0, width)
+      val y = PApplet.map(i, 0, game.parameters.row, 0, width)
+      fill(c)
+      circle(x + diameter/2, y + diameter/2, diameter)
+    }
+  }
+
+  def drawCounts(): Unit = {
+    val toShow = counts.take(100)
+    val (maxGrass, maxSheep, maxWolf) = toShow.foldLeft((0, 0, 0)){
+      case ((mg, ms, mw), (gr, s, w)) => (Math.max(mg, gr), Math.max(ms, s), Math.max(mw, w))
+    }
+    pushStyle()
+    noFill()
+    strokeWeight(1)
+    stroke(0, 255, 0)
+    beginShape()
+    toShow.zipWithIndex.foreach { case (t, i) =>
+      val x = PApplet.map(toShow.length - i, 0, 100, 0, width)
+      val y = PApplet.map(t._1, 0, maxGrass, height - 5, height - 95)
+      curveVertex(x, y)
+    }
+    endShape()
+
+    stroke(0, 0, 255)
+    beginShape()
+    toShow.zipWithIndex.foreach { case (t, i) =>
+      val x = PApplet.map(toShow.length - i, 0, 100, 0, width)
+      val y = PApplet.map(t._2, 0, maxSheep, height - 5, height - 95)
+      curveVertex(x, y)
+    }
+    endShape()
+
+    stroke(255, 0, 0)
+    beginShape()
+    toShow.zipWithIndex.foreach { case (t, i) =>
+      val x = PApplet.map(toShow.length - i, 0, 100, 0, width)
+      val y = PApplet.map(t._3, 0, maxWolf, height - 5, height - 95)
+      curveVertex(x, y)
+    }
+    endShape()
+
+    popStyle()
+
+  }
+
+  override def draw(): Unit = {
+    background(255)
+    drawGrid()
+    drawCounts()
+    val newCounts = game.update()
+    counts = newCounts :: counts
+  }
 }
 
 object GrassSheepWolf extends PApplet {
