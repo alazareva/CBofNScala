acf3f0e80177abdfb33a32598c117bee8d449eb2
anastasia <alazareva@luciddg.com>
2020-09-24 22:58:16 -0700

    before wrapping boids

--- src/main/scala/beautyofnature/autonomousagents/Boids.scala
+++ src/main/scala/beautyofnature/autonomousagents/Boids.scala
@@ -13,27 +13,25 @@ class Boids extends PApplet {
 
   val w = 640
   val h = 480
-  val num = 40
-  val len = 3
+  val numBoids = 40
+  val boidSize = 3
   val angle = 270f
-  val vangle = 90f
-  val minv = 0.5f
-  val ddt  = 0.95f
-  val dt = 1.0f
-  val rcopy = 60//80
-  val rcent = 30
-  val rviso = 40
-  val rvoid = 20
-  val wcopy = 0.2f
-  val wcent = 0.4f
-  val wviso = 0.8f
-  val wvoid = 1.0f
-  val wrand = 0.0f
-  val maxV = 1.5f
+  val visualAngle = 90f
+  val momentumFactor  = 0.95f
+  val timeStep = 1.0f
+  val radiusCopy = 60//80
+  val radiusCenter = 30
+  val radiusVisualAvoidance = 40
+  val radiusAvoidance = 20
+  val weightCopy = 0.2f
+  val weightCenter = 0.4f
+  val weightVisualAvoidance = 0.8f
+  val weightAvoidance = 1.0f
+  val maxVelocity = 1.5f
 
-  val maxr = List(rviso, rcopy, rcent, rvoid).max
-  val cosangle = Math.cos(Math.toRadians(angle / 2))
-  val cosvangle = Math.cos(Math.toRadians(vangle / 2))
+  val maxRadius = List(radiusVisualAvoidance, radiusCopy, radiusCenter, radiusAvoidance).max
+  val cosAngle = Math.cos(Math.toRadians(angle / 2))
+  val cosVisualAngle = Math.cos(Math.toRadians(visualAngle / 2))
 
   def randomBoid: Boid = Boid(
     new PVector(Random.nextInt(w - 50), Random.nextInt(h - 50)),
@@ -41,7 +39,7 @@ class Boids extends PApplet {
     new PVector(0, 0)
     )
 
-  val boids: Array[Boid] = Array.fill(num)(randomBoid)
+  val boids: Array[Boid] = Array.fill(numBoids)(randomBoid)
 
   override def settings(): Unit = {
     size(w, h, PConstants.P2D)
@@ -61,6 +59,21 @@ class Boids extends PApplet {
     if (PVector.dot(candidate, inDirectionOf) < 0) candidate.rotate(PConstants.PI) else candidate
   }
 
+  def otherBoidPosition(thisBoid: Boid, otherBoid: Boid): PVector = {
+    val other = otherBoid.position
+    val current = thisBoid.position
+    val candidates = for {
+      j <- List(-w, 0, w)
+      k <- List(-h, 0, h)
+    } yield PVector.add(other,  new PVector(j, k))
+    candidates.foldLeft((other, PVector.dist(other, current))){ case (best, candidate) =>
+      val (bestV, minDist) = best
+      val d = PVector.dist(candidate, current)
+      if (d < minDist) (candidate, d) else best
+    }._1
+  }
+
+
   def computeBoidHeading(i: Int): Unit = {
     var numCentered = 0
     val a = new PVector(0, 0)
@@ -74,22 +87,20 @@ class Boids extends PApplet {
     } {
       val secondBoid = boids(j)
       val distance = PVector.dist(firstBoid.position, secondBoid.position)
-      if (distance <= maxr) {
+      if (distance <= maxRadius) {
         val vector = PVector.sub(secondBoid.position, firstBoid.position)
         val cos = Math.cos(PVector.angleBetween(firstBoid.velocity, vector))
-        if (cos >= cosangle) {
-          if(distance <= rcent && distance > rvoid) {
+        if (cos >= cosAngle) {
+          if(distance <= radiusCenter && distance > radiusAvoidance) {
             a.add(vector)
             numCentered += 1
           }
-          if (distance <= rcopy && distance > rvoid) b.add(secondBoid.velocity)
-          if (distance <= rvoid) c.add(PVector.sub(firstBoid.position, secondBoid.position).normalize())
-          if (distance <= rviso && cosvangle < cos) {
+          if (distance <= radiusCopy && distance > radiusAvoidance) b.add(secondBoid.velocity)
+          if (distance <= radiusAvoidance) c.add(PVector.sub(firstBoid.position, secondBoid.position).normalize())
+          if (distance <= radiusVisualAvoidance && cosVisualAngle < cos) {
             val dist = PVector.sub(firstBoid.position, secondBoid.position)
-            val orth = orthogonal(dist, firstBoid.velocity)
             val divBy = if (dist.mag() == 0) 1 else dist.mag()
-            val diff  = PVector.add(dist, orth).div(divBy)
-            d.add(diff)
+            d.add(PVector.add(dist, orthogonal(dist, firstBoid.velocity)).div(divBy))
           }
         }
       }
@@ -99,16 +110,19 @@ class Boids extends PApplet {
     b.normalize()
     c.normalize()
     d.normalize()
-    val vt = a.mult(wcent).add(b.mult(wcopy)).add(c.mult(wvoid)).add(d.mult(wviso))
-    val newVelocity = firstBoid.velocity.mult(ddt).add(vt.mult(1 - ddt))
-    firstBoid.newVelocity.set(newVelocity.setMag(maxV))
+    val vt = a.mult(weightCenter)
+      .add(b.mult(weightCopy))
+      .add(c.mult(weightAvoidance))
+      .add(d.mult(weightVisualAvoidance))
+    val newVelocity = firstBoid.velocity.mult(momentumFactor).add(vt.mult(1 - momentumFactor))
+    firstBoid.newVelocity.set(newVelocity.setMag(maxVelocity))
   }
 
   def update(): Unit = {
     boids.indices.foreach(computeBoidHeading)
     boids.foreach{ boid =>
       boid.velocity.set(boid.newVelocity)
-      boid.position.add(boid.velocity.mult(dt))
+      boid.position.add(boid.velocity.mult(timeStep))
       if (boid.position.x < 0) boid.position.x += width
       else if (boid.position.x >= w) boid.position.x -= width
       if (boid.position.y < 0) boid.position.y += h
@@ -122,9 +136,9 @@ class Boids extends PApplet {
     translate(boid.position.x, boid.position.y)
     rotate(boid.velocity.heading() + PConstants.HALF_PI)
     beginShape(PConstants.TRIANGLES)
-    vertex(0, -len * 2)
-    vertex(-len, len * 2)
-    vertex(len, len * 2)
+    vertex(0, -boidSize * 2)
+    vertex(-boidSize, boidSize * 2)
+    vertex(boidSize, boidSize * 2)
     endShape()
     popMatrix()
   }
@@ -137,7 +151,6 @@ class Boids extends PApplet {
     background(255)
     boids.foreach(drawBoid)
     update()
-    //if (frameCount == 6) noLoop()
   }
 }
 
