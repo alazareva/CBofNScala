08f8426ea183264f353f2b2e8982884526f30aa3
anastasia <alazareva@luciddg.com>
2020-09-25 18:20:58 -0700

    working genetic

--- src/main/scala/beautyofnature/evolution/GAString.scala
+++ src/main/scala/beautyofnature/evolution/GAString.scala
@@ -6,10 +6,9 @@ import processing.core.{PApplet, PConstants}
 import scala.util.Random
 
 
-
 case class Individual(s: String) {
   def fitness(target: String): Double = {
-    val fit = s.zip(target).foldLeft(0){case (f, (c1, c2)) => if (c1 == c2) f + 1 else f}
+    val fit = s.zip(target).foldLeft(0) { case (f, (c1, c2)) => if (c1 == c2) f + 1 else f }
     Math.pow(2, fit - target.length)
   }
 }
@@ -24,16 +23,18 @@ class Population(populationSize: Int, crossoverRate: Float, mutationRate: Float,
   def fitness: Array[Double] = {
     val rawFitness = individuals.map(_.fitness(target))
     val total = rawFitness.sum
-    rawFitness.map(_ / total)
+    if (total == 0) rawFitness else rawFitness.map(_ / total)
   }
 
   def selectOne(populationFitness: Array[Double]): Individual = {
     val p = Random.nextDouble
+
     def rec(i: Int, sum: Double): Individual = {
       if (i >= individuals.length) individuals(individuals.length - 1)
       else if (p <= sum + populationFitness(i)) individuals(i)
       else rec(i + 1, sum + populationFitness(i))
     }
+
     rec(0, 0.0)
   }
 
@@ -49,7 +50,7 @@ class Population(populationSize: Int, crossoverRate: Float, mutationRate: Float,
   def reproduce(individual1: Individual, individual2: Individual): Array[Individual] = {
     val crossoverPoint = if (Random.nextFloat < crossoverRate) Random.nextInt(target.length) else target.length
     val s1 = individual1.s.take(crossoverPoint) + individual2.s.drop(crossoverPoint)
-    val s2 = individual2.s. take(crossoverPoint) + individual1.s.drop(crossoverPoint)
+    val s2 = individual2.s.take(crossoverPoint) + individual1.s.drop(crossoverPoint)
 
     Array(
       Individual(s1.map(mutate)),
@@ -57,7 +58,7 @@ class Population(populationSize: Int, crossoverRate: Float, mutationRate: Float,
     )
   }
 
-  def reproduceTwo(populationFitness: Array[Double]): Array[Individual]  = {
+  def reproduceTwo(populationFitness: Array[Double]): Array[Individual] = {
     val individual1 = selectOne(populationFitness)
     val individual2 = selectOne(populationFitness)
     reproduce(individual1, individual2)
@@ -77,21 +78,31 @@ object Population {
     new Population(populationSize, crossoverRate, mutationRate, target)
 }
 
+case class GAStringConfig(target: String, mutationRate: Float, crossoverRate: Float, populationSize: Int, showTop: Int)
+
+object GAStringConfigs {
+  val corgis = GAStringConfig(
+    "welsh corgis have historically been used as herding dogs specifically for cattle",
+    0.01f,
+    0.75f,
+    500,
+    5
+  )
+}
+
 class GAString extends PApplet {
 
-  val target = "i love dogs"
-  val mutationRate = 0.75f
-  val crossoverRate = 0.01f
-  val populationSize = 500
-  val showTop = 3
+  val config = GAStringConfigs.corgis
 
-  val population = Population(populationSize, crossoverRate, mutationRate, target)
+  val population = Population(config.populationSize, config.crossoverRate, config.mutationRate, config.target)
 
   override def settings(): Unit = {
     size(600, 600, PConstants.P2D)
   }
 
   override def setup(): Unit = {
+    background(255)
+    textSize(10)
     fill(0)
   }
 
@@ -99,11 +110,11 @@ class GAString extends PApplet {
     population.next()
     if (frameCount % 5 == 0) {
       background(255)
-      val top = population.population.sortBy(_.fitness(target)).reverse.take(showTop)
+      val top = population.population.sortBy(_.fitness(config.target)).reverse.take(config.showTop)
       top.zipWithIndex.foreach { case (ind, i) =>
-        text(f"${ind.s} : ${ind.fitness(target)}%1.5f", 50, 50 + 100 * i)
+        text(f"${i + 1}. '${ind.s}', Fitness:  ${ind.fitness(config.target)}%1.3f", 50, 50 + 100 * i)
       }
-      if (top.head.s == target) noLoop()
+      if (top.head.s == config.target) noLoop()
     }
   }
 
@@ -115,4 +126,3 @@ object GAString extends PApplet {
     PApplet.main("beautyofnature.evolution.GAString")
   }
 }
-
